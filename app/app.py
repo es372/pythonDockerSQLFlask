c3d1fdd3301d827e59df8c8331b6776c76d5976c
es372 <es372@njit.edu>
2021-07-15 22:27:54 -0400

    Task: Fixed HTML Flask/Database Error

--- app/app.py
+++ app/app.py
@@ -32,13 +32,14 @@ def index():
     user = {'username': 'Miguel'}
     cities_data = cities_import()
 
-    return render_template('layout.html', title='Home', user=user, cities=cities_data)
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
 
 
 @app.route('/api/cities')
 def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
+
     return resp
 
 
